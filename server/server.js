0bbc41ab130851064beefd55a890fd1d258a9203
gameboxtopup <gameboxtopup@gmail.com>
2026-08-03 22:48:18 +0545

    Fix CORS for Vercel

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ const productItemRoutes = require("./routes/productItemRoutes");
 const app = express();
 
 app.use(cors({
-    origin: "http://127.0.0.1:5500",
+    origin: [
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "https://kavronepal.vercel.app"
+    ],
     methods: [
         "GET",
         "POST",
@@ -55,7 +59,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://127.0.0.1:5500",
+        origin: [
+            "http://127.0.0.1:5500",
+            "http://localhost:5500",
+            "https://kavronepal.vercel.app"
+        ],
         methods: ["GET", "POST", "PATCH"]
     }
 });
