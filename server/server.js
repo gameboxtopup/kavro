c53c3868bfec5cc5062799b6219c191cf9487267
gameboxtopup <gameboxtopup@gmail.com>
2026-08-08 09:10:25 +0545

    Increase order payload limit

--- server/server.js
+++ server/server.js
@@ -31,10 +31,7 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({
-    limit: "10mb",
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/api/orders", orderRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/products", productRoutes);
